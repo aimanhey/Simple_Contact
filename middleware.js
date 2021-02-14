96e48077e30046fa781cf5886509cb6d276f0477
aimanhey <43110074+aimanhey@users.noreply.github.com>
2021-02-14 10:57:02 +0800

    update

--- middleware.js
+++ middleware.js
@@ -1,37 +1,31 @@
-const jwt =require('jsonwebtoken');
-const asyncHandler=require('express-async-handler')
-const User=require('./models/user.js')
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("./models/user.js");
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token
+  let token;
   const tokenss = req.headers["x-auth-token"];
 
-  
-  if (
-   tokenss &&
-   tokenss.startsWith('Bearer')
-  ) {
+  if (tokenss && tokenss.startsWith("Bearer")) {
     try {
-      token = tokenss.split(' ')[1]
+      token = tokenss.split(" ")[1];
 
-      const decoded = jwt.verify(token,'secretkey')
-console.log(decoded);
-      req.user = await User.findById(decoded.user)
-console.log(req.user);
+      const decoded = jwt.verify(token, "secretkey");
+      console.log(decoded);
+      req.user = await User.findById(decoded.user);
+      console.log(req.user);
       next();
     } catch (error) {
-      console.error(error)
-      res.status(401)
-      throw new Error('Not authorized, token failed')
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
   }
 
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
-})
+});
 
-
-
-module.exports = protect
+module.exports = protect;
--- models/post.js
+++ models/post.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 //const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     post: {
       type: String,
-      minlength: 2
+      minlength: 2,
     },
     contactNumber: Number,
-    user:{
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'user'
+      ref: "user",
     },
- },
- 
- {
+  },
+
+  {
     timestamps: true,
-    
-  });
-
- 
-
-
+  }
+);
 
 // Create the model class
-const Post = mongoose.model('post', userSchema);
+const Post = mongoose.model("post", userSchema);
 
 // Export the model
 module.exports = Post;
